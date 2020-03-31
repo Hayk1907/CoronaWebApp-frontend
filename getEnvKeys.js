61b122dc574a144e843bd78c57219a207fc9d70f
Hayk <hayk_1998@yahoo.com>
2020-03-31 09:19:49 +0400

    prod mode

--- getEnvKeys.js
+++ getEnvKeys.js
@@ -1,16 +1,5 @@
-const dotenv = require('dotenv');
-const fs = require('fs');
-const path = require('path');
+require('dotenv').config();
 
-module.exports = env => {
-  const basePath = path.join(__dirname, '.env');
-  const envPath = `${basePath}.${env.NODE_ENV}`;
-  const finalPath = fs.existsSync(envPath) ? envPath : basePath;
-
-  const { parsed: fileEnv } = dotenv.config({ path: finalPath });
-
-  return Object.keys(fileEnv).reduce((prev, next) => {
-    prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
-    return prev;
-  }, {});
+module.exports = {
+  key : process.env.G_KEY
 };
