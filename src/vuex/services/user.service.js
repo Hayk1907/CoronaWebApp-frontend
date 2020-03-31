b441fbfa0fb2bb2a641ca4ccb6a3e8f89513eba6
Hayk <hayk_1998@yahoo.com>
2020-03-31 10:50:50 +0400

    refactor

--- src/vuex/services/user.service.js
+++ src/vuex/services/user.service.js
@@ -19,22 +19,3 @@ export default class UserService {
     return data;
   }
 }
-
-
-//"start:prod": "babel-node server.js"
-// const dotenv = require('dotenv');
-// const fs = require('fs');
-// const path = require('path');
-
-// module.exports = env => {
-//   const basePath = path.join(__dirname, '.env');
-//   const envPath = `${basePath}.${env.NODE_ENV}`;
-//   const finalPath = fs.existsSync(envPath) ? envPath : basePath;
-
-//   const { parsed: fileEnv } = dotenv.config({ path: finalPath });
-
-//   return Object.keys(fileEnv).reduce((prev, next) => {
-//     prev[`process.env.${next}`] = JSON.stringify(fileEnv[next]);
-//     return prev;
-//   }, {});
-// };
